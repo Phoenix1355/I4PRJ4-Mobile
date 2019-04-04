be8549a3b2e587729a1a39ffd015c98e99b88ce0
Frank Andersen <frank.andersen@gmail.com>
2019-04-04 14:47:12 +0200

    Added TestFixture for LocationSessionService. Cannot be unit tested off-device.

--- i4prj.SmartCab.UnitTests/Services/LocalSessionServiceTests.cs
+++ i4prj.SmartCab.UnitTests/Services/LocalSessionServiceTests.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using i4prj.SmartCab.Interfaces;
-using i4prj.SmartCab.Responses;
+﻿using i4prj.SmartCab.Interfaces;
 using i4prj.SmartCab.Services;
-using i4prj.SmartCab.UnitTests.Mocks;
-using Newtonsoft.Json;
-using NSubstitute;
 using NUnit.Framework;
 
 namespace i4prj.SmartCab.UnitTests.Services
@@ -17,21 +7,6 @@ namespace i4prj.SmartCab.UnitTests.Services
     [TestFixture]
     public class LocalSessionServiceTests
     {
-
-
-        [SetUp]
-        public void SetUp()
-        {
-
-        }
-
-
-        [Test]
-        public async Task SubmitCreateCustomerCommand_ReceivesValidJsonResponse_ReturnsExpectedValue()
-        {
-
-        }
-
-
+        // Cannot be unit tested because PrismApplicationBase cannot run off-device
     }
 }
