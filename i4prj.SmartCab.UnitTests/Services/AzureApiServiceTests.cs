4fb9d3c177b0e9ea13bac741670d3c2c9001edf3
Frank Andersen <frank.andersen@gmail.com>
2019-04-04 14:19:57 +0200

    Finalized test cases for AzureApiService

--- i4prj.SmartCab.UnitTests/Services/AzureApiServiceTests.cs
+++ i4prj.SmartCab.UnitTests/Services/AzureApiServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -28,9 +29,11 @@ namespace i4prj.SmartCab.UnitTests.Services
             _fakeSessionService = Substitute.For<ISessionService>();
         }
 
-        // Adapted from: http://anthonygiretti.com/2018/09/06/how-to-unit-test-a-class-that-consumes-an-httpclient-with-ihttpclientfactory-in-asp-net-core/
+        // The following HttpClient tests have been adapted from: http://anthonygiretti.com/2018/09/06/how-to-unit-test-a-class-that-consumes-an-httpclient-with-ihttpclientfactory-in-asp-net-core/
+
+        // Success response
         [Test]
-        public async Task SubmitCreateCustomerCommand_WithRequest_ReturnsExpectedResponseType()
+        public async Task SubmitCreateCustomerCommand_ReceivesValidJsonResponse_ReturnsExpectedValue()
         {
             // Arange
             // Fake Http Response
@@ -39,9 +42,17 @@ namespace i4prj.SmartCab.UnitTests.Services
                 StatusCode = HttpStatusCode.OK,
                 Content = new StringContent(JsonConvert.SerializeObject(new
                 {
-                    token = "Test"
+                    token = "Some Valid Token",
+                    customer = new
+                    {
+                        name = "Some name",
+                        email = "email@somehost.com",
+                        phone = "12345678"
+
+                    }
                 }), Encoding.UTF8, "application/json")
             };
+
 
             // Fake HttpHandler
             var fakeHttpMessageHandler = new FakeHttpMessageHandler(fakeHttpResponse);
@@ -49,15 +60,63 @@ namespace i4prj.SmartCab.UnitTests.Services
             // Fake HttpClient
             var fakeHttpClient = new HttpClient(fakeHttpMessageHandler);
 
-            // UUT
             var uut = new AzureApiService(fakeHttpClient, _fakeSessionService);
 
             // Act
             var response = await uut.SubmitCreateCustomerRequest(_fakeCreateCustomerRequest);
 
             // Assert
-            Assert.That(response, Is.Not.Null);
-            Assert.That(response, Is.TypeOf<CreateCustomerResponse>());
+            Assert.Multiple(() => {
+                Assert.That(response, Is.Not.Null);
+                Assert.That(response, Is.TypeOf<CreateCustomerResponse>());
+
+                Assert.That(response.Body, Is.Not.Null);
+                Assert.That(response.Body, Is.TypeOf<CreateCustomerResponseBody>());
+            });
+        }
+
+        // Bad request response
+        [Test]
+        public async Task SubmitCreateCustomerCommand_ReceivesBadRequestResponse_ReturnsExpectedValue()
+        {
+            // Arange
+            // Fake Http Response
+            var fakeHttpResponse = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Content = new StringContent(JsonConvert.SerializeObject(new
+                {
+                    token = (string) null,
+                    customer = (object) null,
+                    errors = new Dictionary<string, IList<string>>() {
+                        { "name", new List<string>() { "Too short" } },
+                        { "email", new List<string>() { "Invalid format", "unknown tld" } }
+                    }
+                }), Encoding.UTF8, "application/json")
+            };
+
+
+            // Fake HttpHandler
+            var fakeHttpMessageHandler = new FakeHttpMessageHandler(fakeHttpResponse);
+
+            // Fake HttpClient
+            var fakeHttpClient = new HttpClient(fakeHttpMessageHandler);
+
+            var uut = new AzureApiService(fakeHttpClient, _fakeSessionService);
+
+            // Act
+            var response = await uut.SubmitCreateCustomerRequest(_fakeCreateCustomerRequest);
+
+            // Assert
+            Assert.Multiple(() => {
+                Assert.That(response, Is.Not.Null);
+                Assert.That(response, Is.TypeOf<CreateCustomerResponse>());
+
+                Assert.That(response.Body, Is.Not.Null);
+                Assert.That(response.Body, Is.TypeOf<CreateCustomerResponseBody>());
+
+                Assert.That(response.GetErrors().Count, Is.EqualTo(3));
+            });
         }
     }
 }
