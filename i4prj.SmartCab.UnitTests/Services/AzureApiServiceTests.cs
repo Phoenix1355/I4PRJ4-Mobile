39254044d91c5f68c37b94ff1fd74f6eb9b89188
Frank Andersen <frank.andersen@gmail.com>
2019-04-11 15:01:08 +0200

    Cleaned up AzureApiServiceTests

--- i4prj.SmartCab.UnitTests/Services/AzureApiServiceTests.cs
+++ i4prj.SmartCab.UnitTests/Services/AzureApiServiceTests.cs
@@ -20,6 +20,10 @@ namespace i4prj.SmartCab.UnitTests.Services
         private ICreateCustomerRequest _fakeCreateCustomerRequest;
         private ILoginRequest _fakeLoginRequest;
         private ISessionService _fakeSessionService;
+        private FakeHttpMessageHandler _fakeHttpMessageHandler;
+        private HttpClient _fakeHttpClient;
+
+        private AzureApiService _uut;
 
         [SetUp]
         public void SetUp()
@@ -27,43 +31,30 @@ namespace i4prj.SmartCab.UnitTests.Services
             _fakeCreateCustomerRequest = Substitute.For<ICreateCustomerRequest>();
             _fakeLoginRequest = Substitute.For<ILoginRequest>();
             _fakeSessionService = Substitute.For<ISessionService>();
+
+            _fakeHttpMessageHandler = new FakeHttpMessageHandler();
+            _fakeHttpClient = new HttpClient(_fakeHttpMessageHandler);
+
+            _uut = new AzureApiService(_fakeHttpClient, _fakeSessionService);
         }
 
         // The following HttpClient tests have been adapted from: http://anthonygiretti.com/2018/09/06/how-to-unit-test-a-class-that-consumes-an-httpclient-with-ihttpclientfactory-in-asp-net-core/
 
-        // Success response
+        /// <summary>
+        /// Test to see whether a successfull response with valid json will be transformed correctly
+        /// </summary>
         [Test]
-        public async Task SubmitCreateCustomerCommand_ReceivesValidJsonResponse_ReturnsExpectedValue()
+        public async Task SubmitCreateCustomerRequest_ReceivesValidJsonResponse_ReturnsExpectedValue()
         {
-            // Arange
-            // Fake Http Response
-            var fakeHttpResponse = new HttpResponseMessage()
+            // Arrange: Fake Http Response
+            _fakeHttpMessageHandler.FakeResponse = new HttpResponseMessage()
             {
                 StatusCode = HttpStatusCode.OK,
-                /*
-                {
-                    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJmcmFuay5hbmRlcnNlbkBnbWFpbC5jb20iLCJVc2VySWQiOiJiNzI1ZDc3Yy1hNmI3LTRiMmItYjA5Ni1iNzEwMDcwY2NmNzgiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJDdXN0b21lciIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjA0LzA0LzIwMTkgMTQ6NDk6MDkiLCJleHAiOjE1NTQzODkzNDl9.eWRgwE2orLoLV88-9xtM_sbQfdh8Sy3HtAH1ersBhkE",
-                    "customer": {
-                        "name": "Frank Andersen",
-                        "email": "frank.andersen@gmail.com",
-                        "phoneNumber": "31133165"
-                    }
-                }               
-                */
                 Content = new StringContent("{\n\t\"token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJmcmFuay5hbmRlcnNlbkBnbWFpbC5jb20iLCJVc2VySWQiOiJiNzI1ZDc3Yy1hNmI3LTRiMmItYjA5Ni1iNzEwMDcwY2NmNzgiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJDdXN0b21lciIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjA0LzA0LzIwMTkgMTQ6NDk6MDkiLCJleHAiOjE1NTQzODkzNDl9.eWRgwE2orLoLV88-9xtM_sbQfdh8Sy3HtAH1ersBhkE\",\n\t\"customer\": {\n\t\t\"name\": \"Frank Andersen\",\n\t\t\"email\": \"frank.andersen@gmail.com\",\n\t\t\"phoneNumber\": \"31133165\"\n\t}\n}", Encoding.UTF8, "application/json")
             };
 
-
-            // Fake HttpHandler
-            var fakeHttpMessageHandler = new FakeHttpMessageHandler(fakeHttpResponse);
-
-            // Fake HttpClient
-            var fakeHttpClient = new HttpClient(fakeHttpMessageHandler);
-
-            var uut = new AzureApiService(fakeHttpClient, _fakeSessionService);
-
             // Act
-            var response = await uut.SubmitCreateCustomerRequest(_fakeCreateCustomerRequest);
+            var response = await _uut.SubmitCreateCustomerRequest(_fakeCreateCustomerRequest);
 
             // Assert
             Assert.Multiple(() => {
@@ -75,35 +66,75 @@ namespace i4prj.SmartCab.UnitTests.Services
             });
         }
 
-        // Bad request response
+        /// <summary>
+        /// Test to see whether an unsuccessfull response with valid json will be transformed correctly
+        /// </summary>
         [Test]
-        public async Task SubmitCreateCustomerCommand_ReceivesBadRequestResponse_ReturnsExpectedValue()
+        public async Task SubmitCreateCustomerRequest_ReceivesBadRequestResponse_ReturnsExpectedValue()
         {
-            // Arange
-            // Fake Http Response
-            var fakeHttpResponse = new HttpResponseMessage()
+            // Arrange: Fake Http Response
+            _fakeHttpMessageHandler.FakeResponse = new HttpResponseMessage()
             {
                 StatusCode = HttpStatusCode.BadRequest,
-                Content = new StringContent(JsonConvert.SerializeObject(new
-                {
-                    token = (string)null,
-                    customer = (object)null,
-                    errors = new Dictionary<string, IList<string>>() {
-                        { "error", new List<string>() { "User name is already taken" } }
-                    }
-                }), Encoding.UTF8, "application/json")
+                Content = new StringContent("{\n\t\"token\": null,\n\t\"customer\": \"\",\n\t\"errors\": {\n\t\t\"error\": [\"User name is already taken\"]\n\t}\n}")
             };
 
-            // Fake HttpHandler
-            var fakeHttpMessageHandler = new FakeHttpMessageHandler(fakeHttpResponse);
+            // Act
+            var response = await _uut.SubmitCreateCustomerRequest(_fakeCreateCustomerRequest);
 
-            // Fake HttpClient
-            var fakeHttpClient = new HttpClient(fakeHttpMessageHandler);
+            // Assert
+            Assert.Multiple(() => {
+                Assert.That(response, Is.Not.Null);
+                Assert.That(response, Is.TypeOf<CreateCustomerResponse>());
 
-            var uut = new AzureApiService(fakeHttpClient, _fakeSessionService);
+                Assert.That(response.Body, Is.Not.Null);
+                Assert.That(response.Body, Is.TypeOf<CreateCustomerResponseBody>());
+
+                Assert.That(response.GetErrors().Count, Is.EqualTo(1));
+            });
+        }
+
+        /// <summary>
+        /// Test to see whether a successfull response with valid json will be transformed correctly
+        /// </summary>
+        [Test]
+        public async Task SubmitLoginRequest_ReceivesValidJsonResponse_ReturnsExpectedValue()
+        {
+            // Arrange: Fake Http Response
+            _fakeHttpMessageHandler.FakeResponse = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent("{\n  \"token\": \"tokenValue\",\n  \"customer\": {\n    \"name\": \"customer name\",\n    \"email\": \"some@email.com\",\n    \"phoneNumber\": \"12345678\"\n  }\n}", Encoding.UTF8, "application/json")
+            };
 
             // Act
-            var response = await uut.SubmitCreateCustomerRequest(_fakeCreateCustomerRequest);
+            var response = await _uut.SubmitLoginRequest(_fakeLoginRequest);
+
+            // Assert
+            Assert.Multiple(() => {
+                Assert.That(response, Is.Not.Null);
+                Assert.That(response, Is.TypeOf<LoginResponse>());
+
+                Assert.That(response.Body, Is.Not.Null);
+                Assert.That(response.Body, Is.TypeOf<LoginResponseBody>());
+            });
+        }
+
+        /// <summary>
+        /// Test to see whether an unsuccessfull response with valid json will be transformed correctly
+        /// </summary>
+        [Test]
+        public async Task SubmitLoginRequest_ReceivesBadRequestResponse_ReturnsExpectedValue()
+        {
+            // Arrange: Fake Http Response
+            _fakeHttpMessageHandler.FakeResponse = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Content = new StringContent("{\n\t\"token\": null,\n\t\"customer\": null,\n\t\"errors\": {\n\t\t\"error\": [\"Bad credentials\"]\n\t}\n}", Encoding.UTF8, "application/json")
+            };
+
+            // Act
+            var response = await _uut.SubmitCreateCustomerRequest(_fakeCreateCustomerRequest);
 
             // Assert
             Assert.Multiple(() => {
