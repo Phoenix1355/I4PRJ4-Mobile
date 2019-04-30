5752fa1b1ae1fc6c4926271e15fa296d196cb184
Frank Andersen <frank.andersen@gmail.com>
2019-04-30 10:56:12 +0200

    EditAccountResponse partiotioned and altered to use DTO

---
+++ i4prj.SmartCab/i4prj.SmartCab/Responses/EditAccountResponseBody.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using i4prj.SmartCab.Converters;
+using i4prj.SmartCab.Interfaces;
+using i4prj.SmartCab.Models;
+using Newtonsoft.Json;
+
+namespace i4prj.SmartCab.Responses
+{
+    public class EditAccountResponseBody : BaseResponseBody
+    {
+        [JsonConverter(typeof(ConcreteConverter<CustomerDTO>))]
+        public ICustomerDTO customer { get; set; }
+    }
+}
